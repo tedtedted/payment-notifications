33430c847c23f48cf7ebeebe7ff2f6df566a1fae
Ted Redington <ted.redington@adyen.com>
2020-06-22 21:36:43 -0400

    add /reports controller

---
+++ src/main/java/com/tedredington/AdyenNotifications/controller/ReportsController.java
@@ -0,0 +1,29 @@
+package com.tedredington.AdyenNotifications.controller;
+
+import com.adyen.model.notification.NotificationRequest;
+import com.tedredington.AdyenNotifications.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/adyen")
+public class ReportsController {
+
+    @Autowired
+    NotificationService notificationService;
+
+    @PostMapping(value="/reports", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> postReports() {
+
+        return ResponseEntity.ok("[accepted]");
+    }
+
+    @GetMapping(value="/reports")
+    public ResponseEntity<String> getReports () {
+
+        return ResponseEntity.ok("OK");
+    }
+
+}
