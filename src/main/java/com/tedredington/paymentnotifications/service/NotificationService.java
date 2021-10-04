b95d5df7fb683b9ae05a72f59dbc57e68e9998fd
tedtedted <tedredington@gmail.com>
2021-10-03 20:33:28 -0400

    Adding in Support for Mongo

--- src/main/java/com/tedredington/paymentnotifications/service/NotificationService.java
+++ src/main/java/com/tedredington/paymentnotifications/service/NotificationService.java
@@ -1,20 +1,21 @@
 package com.tedredington.paymentnotifications.service;
 
 import com.adyen.model.notification.NotificationRequest;
-import com.adyen.model.notification.NotificationRequestItem;
-import com.adyen.util.HMACValidator;
-import com.tedredington.paymentnotifications.domain.EventTypes;
 import com.tedredington.paymentnotifications.domain.HmacStatus;
+import com.tedredington.paymentnotifications.repo.NotificationRepository;
 import com.tedredington.paymentnotifications.utils.HmacValidation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.security.SignatureException;
-import java.util.List;
 
 @Service
 public class NotificationService {
+
+    @Autowired
+    NotificationRepository notificationItemRepo;
 
     Logger logger = LoggerFactory.getLogger(NotificationService.class);
 
@@ -25,10 +26,17 @@ public class NotificationService {
 
         HmacStatus status = HmacValidation.validateHmac(notificationRequest);
 
+        Logger logger = LoggerFactory.getLogger(NotificationService.class);
+
+        // Fake HMAC from the Adyen docs
+        String hmacKey = "b0ea55c2fe60d4d1d605e9c385e0e7";
+
         if (status.equals(HmacStatus.NOT_VALID)) {
-            logger.info("HMAC Status: " + status +  ". Not Moving Forward with Sending");
+            logger.info("HMAC Status: " + status + ". Not Moving Forward with Sending");
         } else {
             logger.info("HMAC Status: " + status + ". Moving Forward with Sending to Queue");
+            notificationItemRepo.save(notificationRequest);
+
         }
     }
 }
