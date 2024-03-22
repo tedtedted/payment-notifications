38db61e95711a5d4731cee7b453aa9b0ce384ddb
Ted Redington <tedredington@gmail.com>
2024-03-22 18:14:55 -0500

    rename main Application class

--- src/main/java/com/tedredington/paymentnotifications/Application.java
+++ src/main/java/com/tedredington/paymentnotifications/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentNotificationsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaymentNotificationsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
